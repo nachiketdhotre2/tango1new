78a54db6c4775e77467d38e484cf9f1747aaec7c
nachiket <nachiket.dhotre3@gmail.com>
2017-08-03 21:40:21 +0530

    second commit - tango app template and dynamic paths

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello World!!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>.")
